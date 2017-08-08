7cc66be8b51d7fc0defe00f95091e326209e0812
Shien Yang Lee <Shien Yang Lee>
2017-08-08 12:27:57 -0400

    Shorten lines to fit appendix

--- libs/python-gcode/gcode.py
+++ libs/python-gcode/gcode.py
@@ -54,7 +54,10 @@ class Line(object):
 						if re.match('[A-Za-z]', arg[0]):
 							if arg[1:] is not None and arg[1:] != '':
 								try:
-									self.args[arg[0]] = float(arg[1:]) if '.' in arg[1:] else int(arg[1:]) #only convert to float if decimal point present
+									#only convert to float if decimal point present
+									self.args[arg[0]] = float(arg[1:])\
+									 if '.' in arg[1:]\
+									  else int(arg[1:]) 
 								except ValueError:
 									sys.stderr.write("Line: %s\n" % line)
 									sys.stderr.write("Code: %s\n" % self.code)
@@ -69,13 +72,17 @@ class Line(object):
 		[X,Y,Z] = self.initial_point.get_coordinates() #default to no movemente
 		# update with destination specified in current line if applicable
 		if self.code in {'G0','G1'}:
-			if 'X' not in self.args and 'Y' not in self.args and 'Z' not in self.args and 'E' in self.args:
+			if 'X' not in self.args and \
+			'Y' not in self.args and \
+			'Z' not in self.args and \
+			'E' in self.args:
 				self.args = self.args
-				#do not force explicit XYZ for non-moving filament-only moves (they are sometimes in relative mode)
+				#do not force explicit XYZ for filament-only moves (ometimes in relative mode)
 			else:
 				if 'X' in self.args:
 					X = self.args['X']
-				elif explicit: # do not allow implicit non-moving axis, force explicit XYZ dest if explicit specified
+				#force explicit XYZ dest if explicit specified
+				elif explicit: 
 					self.args['X'] = X
 				if 'Y' in self.args:
 					Y = self.args['Y']
@@ -101,9 +108,12 @@ class Line(object):
 
 	def get_length(self):
 		"""returns length of current line in 3-space (XYZ)"""
-		return ((self.final_point.get_coordinates()[0]-self.initial_point.get_coordinates()[0])**2  +\
-				(self.final_point.get_coordinates()[1]-self.initial_point.get_coordinates()[1])**2 + \
-				(self.final_point.get_coordinates()[2]-self.initial_point.get_coordinates()[2])**2)**0.5 # pythagorus' theorem
+		return ((self.final_point.get_coordinates()[0]\
+			     -self.initial_point.get_coordinates()[0])**2  +\
+				(self.final_point.get_coordinates()[1]\
+				 -self.initial_point.get_coordinates()[1])**2 + \
+				(self.final_point.get_coordinates()[2]\
+				 -self.initial_point.get_coordinates()[2])**2)**0.5 # pythagorus' theorem
 
 	def get_code(self):
 		return self.code
@@ -118,13 +128,14 @@ class Line(object):
 		return self.final_point
 
 	def split_move(self,segment_length):
-		"""if current move is longer than segment_length (provided in mm), split into segments of specified length (put single short line from remainder at end)
+		"""if current move is longer than segment_length (provided in mm), 
+		split into segments of specified length (put single short line from remainder at end)
 		and return list of lines. else return single-element list containing self."""
 
 		if self.length <= segment_length:
 			return [self]
 		else:
-			#calculate number of segments into which to split current line; use floor division to put short segment at the end
+			#calculate number of segments into which to split current line; short segment at end
 			n_segments = int(self.length//segment_length)
 
 			#calculate direction cosines
@@ -145,14 +156,22 @@ class Line(object):
 			if self.line == 'G0 X0 Y1000 F10800.0 ':
 				print 'direction: (%f,%f,%f)' % (X_hat,Y_hat,Z_hat)
 				print 'magnitude: %f' % self.length
-				print 'initial: (%f,%f,%f)' % (self.initial_point.get_coordinates()[0],self.initial_point.get_coordinates()[1],self.initial_point.get_coordinates()[2])
-				print 'final: (%f,%f,%f)' % (self.final_point.get_coordinates()[0],self.final_point.get_coordinates()[1],self.final_point.get_coordinates()[2])
+				print 'initial: (%f,%f,%f)' % (
+					self.initial_point.get_coordinates()[0],
+					self.initial_point.get_coordinates()[1],
+					self.initial_point.get_coordinates()[2])
+				print 'final: (%f,%f,%f)' % (
+					self.final_point.get_coordinates()[0],
+					self.final_point.get_coordinates()[1],
+					self.final_point.get_coordinates()[2])
 
 			#calculate extrusion length for each segment
-			E_full_length_segments = round(self.args['E'] * (float(segment_length)/self.length),3) #distribute extruded filament length by print length
-			E_last_segment = round(self.args['E'] * ((self.length - (float(segment_length)*n_segments))/self.length),3)
+			E_full_length_segments = round(self.args['E'] *\
+				(float(segment_length)/self.length),3) #distribute filament length by print length
+			E_last_segment = round(self.args['E'] *\
+				((self.length - (float(segment_length)*n_segments))/self.length),3)
 
-			#instantiate first line (this is redundant move with zero length; just stays at initial position of original unsplit line)
+			#instantiate first line (this is redundant move with zero length)
 			first_line_args = copy.deepcopy(self.args)
 			first_line_args['X'] = self.get_initial_point().get_coordinates()[0]
 			first_line_args['Y'] = self.get_initial_point().get_coordinates()[1]
@@ -164,39 +183,48 @@ class Line(object):
 										 first_line_args,\
 										 'begin splt')]
 
-			for i in range(1,n_segments+1): #start at i=1 to avoid redundant line instructing machine to stay at initial point
+			for i in range(1,n_segments+1): #exclude redundant first line
 				cur_X = round(self.initial_point.get_coordinates()[0] + i*segment_length*X_hat,3)
 				cur_Y = round(self.initial_point.get_coordinates()[1] + i*segment_length*Y_hat,3)
 				cur_Z = round(self.initial_point.get_coordinates()[2] + i*segment_length*Z_hat,3)
 
 				#make next segment
-				cur_args = copy.deepcopy(self.args) #copy over arguments from unsplit line to preserve feedrate, extrusion etc.
+				cur_args = copy.deepcopy(self.args) #copy over arguments from unsplit line
 				#update XYZ destination with incremental values
 				cur_args['X'] = cur_X
 				cur_args['Y'] = cur_Y
 				cur_args['Z'] = cur_Z
 				cur_args['E'] = E_full_length_segments
-				if i > 1 and 'F' in cur_args: #explicitly specify feedrate only for first line in sequence of split moves
+				if i > 1 and 'F' in cur_args: #explicitly specify feedrate only for first segment
 					del cur_args['F']
-				incremental_line = Line('', list_of_constituent_lines[-1].get_final_point(), self.code, cur_args, 'splt')
+				incremental_line = Line(
+					'', 
+					list_of_constituent_lines[-1].get_final_point(), 
+					self.code, cur_args, 
+					'splt')
 
 
 				#append next segment to list of split moves
 				list_of_constituent_lines.append(incremental_line)
 
-			#instantiate last time (this segment may be shorter than the specified segment length and is required to bring machine to destination specced in unsplit move)
+			#instantiate last time (this segment may be shorter than the specified segment length)
 			last_line_args = copy.deepcopy(self.args)
 			last_line_args['E'] = E_last_segment
 			if 'F' in last_line_args: #do not explicitly specify feedrate for last line
 				del last_line_args['F']
-			last_line = Line('', list_of_constituent_lines[-1].get_final_point(), self.code, last_line_args, 'end splt')
+			last_line = Line(
+				'', 
+				list_of_constituent_lines[-1].get_final_point(), 
+				self.code, last_line_args, 
+				'end splt')
 			list_of_constituent_lines.append(last_line)
 
 			#remove redundant first line (a redundant line going nowhere causes printer to hesitate)
 			del list_of_constituent_lines[0]
 
 			#sanity check
-			if list_of_constituent_lines[-1].get_final_point().get_coordinates() != self.final_point.get_coordinates():
+			if list_of_constituent_lines[-1].get_final_point().get_coordinates() != \
+			self.final_point.get_coordinates():
 				raise ValueError('Split line not coming back to original destination.')
 
 			return list_of_constituent_lines
@@ -219,17 +247,20 @@ class Layer(object):
 		self.postamble = []
 		self.lines = []
 
-		first_line_in_layer = Line(lines[0],prev_layer_final_pt,explicit=explicit) #make Line object from unsplit first line
+		first_line_in_layer = Line(
+		lines[0],
+		prev_layer_final_pt,
+		explicit=explicit) #make Line object from unsplit first line
 		if first_line_in_layer.get_code() in ['G0','G1'] and split: #only split move lines
-			split_first_line_in_layer = first_line_in_layer.split_move(SEG_LENGTH_SPLIT) #split first line into 1mm-long segments
-			self.lines += split_first_line_in_layer # initialize list of lines with split fist line
+			split_first_line_in_layer = first_line_in_layer.split_move(SEG_LENGTH_SPLIT)
+			self.lines += split_first_line_in_layer # initialize list of lines with split 1st line
 		else:
 			self.lines += [first_line_in_layer]
 
 		for l in lines[1:]:
 			prev_line = self.lines[-1]
 			cur_line = Line(l,prev_line.get_final_point(),explicit=explicit)
-			if cur_line.get_code() in ['G0','G1'] and 'E' in cur_line.get_args() and split: #do not split non-printing moves
+			if cur_line.get_code() in ['G0','G1'] and 'E' in cur_line.get_args() and split:
 				split_cur_line = cur_line.split_move(SEG_LENGTH_SPLIT)
 				self.lines += split_cur_line
 			else:
@@ -254,7 +285,6 @@ class Layer(object):
 	def extents(self):
 		"""Return the extents of the layer: the min/max in x and y that
 		occur. Note this does not take arcs into account."""
-		#TO-DO: This doesn't work correctly for G28 lines where X and Y are passed but without numerical values
 		min_x = min(self.lines, key=lambda l: l.args.get('X', float('inf'))).args['X']
 		min_y = min(self.lines, key=lambda l: l.args.get('Y', float('inf'))).args['Y']
 		max_x = max(self.lines, key=lambda l: l.args.get('X', float('-inf'))).args['X']
@@ -308,12 +338,14 @@ class Layer(object):
 		compensator(). compensator is a Compensator3D instance representing
 		the error model being used for compensation."""
 		for line in self.lines:
-			if line.code in {'G1','G0'}: #apply compensation only to G1 lines (Slic3r generated lines)
+			if line.code in {'G1','G0'}:
 				if 'X' in line.args and 'Y' in line.args: #check if it is an XY move line
 					X = line.args['X']
 					Y = line.args['Y']
-					line.args['Z'] = line.args['Z'] - compensator.get_predicted_error(X-4.195,Y-28.195,self.z()) #apply z compensation with appropriate xy offset
-				elif 'X' in line.args or 'Y' in line.args: #uniaxial traverses (theses are non-print moves)
+					#apply z compensation with appropriate xy offset
+					line.args['Z'] = line.args['Z'] -\
+					 compensator.get_predicted_error(X-4.195,Y-28.195,self.z()) 
+				elif 'X' in line.args or 'Y' in line.args: #uniaxial traverses
 					line.args['Z'] = self.z() #force to original layer height
 			#elif line.code == 'G0': #default G0 lines to original layer height
 				#line.args['Z'] = self.z()
@@ -406,19 +438,30 @@ class Gcode(object):
 				if not l: #skip empty lines
 					continue
 				#Looks like a layer change because we have a Z
-				if re.match(r'G[01]\s+Z-?\.?\d+', l): #this may pose problems when we try to do non-planar layers (e.g. volumeteric error compensation)
+				if re.match(r'G[01]\s+Z-?\.?\d+', l): 
 					if in_preamble:
 						if not in_raft:
-							self.preamble = Layer(Point(0,0,0), curr_layer, split=False, layernum=0, explicit=False) #do not split preamble (not compensating anyway)
-							in_preamble = False # preamble only ends at the first layer change after raft finishes
+							self.preamble = Layer(
+							Point(0,0,0), 
+							curr_layer, 
+							split=False, 
+							layernum=0, 
+							explicit=False) #do not split preamble (not compensating anyway)
+							in_preamble = False #preamble ends at 1st layer change after raft end
 						else:
 							curr_layer.append(l) #append to preamble if still in raft
 							continue #skip rest of loop
 					else:
 						if len(self.layers) == 0:
-							self.layers.append(Layer(self.preamble.get_final_point(), curr_layer, layernum=layernum))
+							self.layers.append(Layer(
+								self.preamble.get_final_point(), 
+								curr_layer, 
+								layernum=layernum))
 						else:
-							self.layers.append(Layer(self.layers[-1].get_final_point(), curr_layer, layernum=layernum))
+							self.layers.append(Layer(
+								self.layers[-1].get_final_point(),
+								curr_layer, 
+								layernum=layernum))
 						layernum =+ 1
 					curr_layer = [l]
 
@@ -428,7 +471,10 @@ class Gcode(object):
 					if l == '; END RAFT':
 						in_raft = False # exit raft once END RAFT flag detected
 
-			self.layers.append(Layer(self.layers[-1].get_final_point(), curr_layer, layernum=layernum))
+			self.layers.append(Layer(
+				self.layers[-1].get_final_point(), 
+				curr_layer, 
+				layernum=layernum))
 
 if __name__ == "__main__":
 	if sys.argv[1:]:
